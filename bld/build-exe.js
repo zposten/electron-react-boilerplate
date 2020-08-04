6f76e4e3f4498ef78ddf8966911fa9f4900fc538
Zach Posten <zach@posten.io>
2020-08-04 11:59:48 -0500

    feat: Successfully created installer for Electron with CRA

--- bld/build-exe.js
+++ bld/build-exe.js
@@ -1,5 +1,6 @@
 const packager = require('electron-packager')
 const fs = require('fs-extra')
+const path = require('path')
 
 const { fromRoot, getPaths, success } = require('./util')
 
@@ -21,10 +22,19 @@ async function createExe() {
     arch: 'x64',
     name: appName,
     executableName: exeNameWithoutExt,
+    // I had to set this to avoid some stupid "path is too long" error when building the installer
+    // See: https://github.com/electron/windows-installer/issues/219
+    asar: true,
   })
 
   let defaultAppDir = appPaths[0]
   await fs.rename(defaultAppDir, desiredAppDir)
-
   success('Created EXE at: ' + desiredAppDir)
+
+  await fs.copy(
+    fromRoot('build'),
+    path.join(desiredAppDir, 'resources', 'app', 'build')
+  )
+
+  success('Copied production React app into built electron app')
 }
